40db5c052887bc2f75248928712fff3b2b80fecf
= <=>
2022-03-09 11:00:20 -0500

    prueba de texto

--- cuadrado/js/iniciar.js
+++ cuadrado/js/iniciar.js
@@ -2,5 +2,6 @@ addEventListener("load", (event)=>{
 	iniciarMovimiento()
 	generarEscenario()
 	detectar()
+	document.getElementById("txts").innerHTML = intro;
 	}, false
 )
---
+++ cuadrado/js/textos.js
@@ -0,0 +1,2 @@
+var txts = document.getElementById("txts");
+var intro = "bienvenido a este juego <br> aqui tendras que llevar al conejo rojo a traves de puzzles para encontrar la salida";
